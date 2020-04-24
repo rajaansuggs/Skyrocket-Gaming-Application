6976860820b4a8a5873967410e9a894c5d7a2116
rajaansuggs <42246673+rajaansuggs@users.noreply.github.com>
2020-04-24 06:02:23 -0400

    Created a JUnit Test for Unit Testing

---
+++ src/LoginTest.java
@@ -0,0 +1,35 @@
+
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+import org.junit.Test;
+
+public class LoginTest {
+	@Test
+	public void canLogin() {
+		User u = null;
+		Scanner scanner = null;
+		ArrayList<Game> games = new ArrayList<Game>();
+		try {
+			scanner = new Scanner(new File("threeToFiveGames.txt"));
+			scanner.nextLine();
+			while(scanner.hasNextLine())
+			{
+				Game game = new Game(scanner);
+				games.add(game);
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		u = new User(1999720968,	"bee",	"cee",	"dee",	"ree", games);
+		boolean verifyLogin2 = u.canLogin("dee", "ree", games);
+		assertTrue(verifyLogin2);
+	}
+
+}
