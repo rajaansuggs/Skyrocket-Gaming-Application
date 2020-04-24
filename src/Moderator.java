48aaa2f98e0593bcb45156c9851578ec7c801272
rajaansuggs <42246673+rajaansuggs@users.noreply.github.com>
2020-04-24 06:14:13 -0400

    Create Moderator.java

---
+++ src/Moderator.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Moderator extends User {
+
+	public Moderator(int userid, String fn, String ln, String uname, String pass, ArrayList<Game> games) {
+		super(userid, fn, ln, uname, pass, games);
+		
+	}
+
+}
