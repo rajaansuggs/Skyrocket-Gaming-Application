0c966bd6ad3b7d42eb868a42c8de5b00e7614158
rajaansuggs <42246673+rajaansuggs@users.noreply.github.com>
2020-04-29 17:55:30 -0400

    more fields for Game objects

--- src/Game.java
+++ src/Game.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class Game {
 	//======================Instance Variables
 	private int gameId;
-	private String gameName, gameDescription, hyperlink;
+	private String gameName, gameDescription, hyperlink, company, versionNumber, compatibleSystem;
+	
+
 	private GameRating rating;
 	
 	//======================Constructors
@@ -12,26 +14,32 @@ public class Game {
 	}
 	
 	public Game(String[] games) {
-		this(Integer.parseInt(games[0]), games[1], games[2], Double.parseDouble(games[3]), games[4]);
+		this(Integer.parseInt(games[0]), games[1], games[2], Double.parseDouble(games[3]), games[4], games[5], games[6], games[7]);
 		
 	}
 	
-	public Game(int gameId, String gameName, String gameDescription, double averageRating, String hyperlink) {
+	public Game(int gameId, String gameName, String gameDescription, double averageRating, String hyperlink, String company, String versionNumber, String compatibleSystem) {
 		setGameId(gameId);
 		setGameName(gameName);
 		setGameDescription(gameDescription);
 		setRating(new GameRating(gameId));
 		setGameHyperLink(hyperlink);
+		setCompany(company);
+		setVersionNumber(versionNumber);
+		setCompatibleSystem(compatibleSystem);
 	}
 
 
 	//======================Methods
+	
+	
 	@Override
 	public String toString() {
-		return "Games gameId=" + gameId + ", gameName=" + gameName + ", gameDescription=" + gameDescription
-				+ ", averageRating=" + rating.getAvgRating() + ", hyperlink=" + hyperlink;
+		return "Game [gameId=" + gameId + ", gameName=" + gameName + ", gameDescription=" + gameDescription
+				+ ", hyperlink=" + hyperlink + ", company=" + company + ", versionNumber=" + versionNumber
+				+ ", compatibleSystem=" + compatibleSystem + ", rating=" + rating + "]";
 	}
-	
+
 	//Value will be truncated to 2 decimal places, then set out of 5 stars
 	public double getStarRating() {
 		double value = rating.getAvgRating();
@@ -80,5 +88,28 @@ public class Game {
 		this.hyperlink = hyperlink;
 	}
 	
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public String getVersionNumber() {
+		return versionNumber;
+	}
+
+	public void setVersionNumber(String versionNumber) {
+		this.versionNumber = versionNumber;
+	}
+
+	public String getCompatibleSystem() {
+		return compatibleSystem;
+	}
+
+	public void setCompatibleSystem(String compatibleSystem) {
+		this.compatibleSystem = compatibleSystem;
+	}
 
 }
