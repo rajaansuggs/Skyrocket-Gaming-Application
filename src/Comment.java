ddf04923e122872fc260cd7601d4659eb3ef30b4
rajaansuggs <42246673+rajaansuggs@users.noreply.github.com>
2020-04-30 10:12:31 -0400

    added userid field to a Comment object

--- src/Comment.java
+++ src/Comment.java
@@ -1,12 +1,39 @@
-import java.util.ArrayList;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 //Good work! Keep going
 public class Comment {
-	private int commentId, gameId;
+	private int commentId, gameId, userId;
 	private String commentString;
-	public Comment(int commentId, int gameId,String commentString) {
+	public Comment(int userId, int commentId, int gameId, String commentString) {
+		setUserId(userId);
 		setCommentId(commentId);
 		setCommentString(commentString);
 		setGameId(gameId);
+		load(userId, commentId, gameId, commentString);
+	}
+	
+	private void load(int userId, int commentId, int gameId, String commentString) {
+		try {
+			FileWriter saveFile = new FileWriter("commentSection.txt", true);
+			BufferedWriter save = new BufferedWriter(saveFile);
+			save.write(userId + "\t");
+			save.write(commentId +"\t");
+			save.write(gameId +"\t");
+			save.write(commentString +"\t");
+			save.newLine();
+			save.close();
+		}
+		catch(Exception e) {
+			
+		}
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
 	}
 	public int getGameId() {
 		return gameId;
@@ -28,3 +55,4 @@ public class Comment {
 	}
 	
 }
+
