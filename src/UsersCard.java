23f967a83269e9b602224874c3a90c2e68fc4312
rajaansuggs <42246673+rajaansuggs@users.noreply.github.com>
2020-05-01 11:47:58 -0400

    user cards updates the user database

--- src/UsersCard.java
+++ src/UsersCard.java
@@ -1,63 +1,28 @@
-import java.awt.Font;
-import java.io.File;
-import java.io.RandomAccessFile;
-import java.util.ArrayList;
-import java.util.Collections;
-
-import javax.swing.JOptionPane;
-import javax.swing.UIManager;
-
-
-
+import java.io.*;
+//=========================Constructor
 public class UsersCard {
-	private ArrayList<User> users = new ArrayList<>();
-	private final static String FILENAME = "user.bin";
-	private String fileName;
-	private int size = 0;
-	public UsersCard(String defaultFilename) {
-		setFileName(defaultFilename);
-		load();
-	}
-	public void setFileName(String fileName) {
-		fileName = fileName.toLowerCase().trim();
-		if(fileName.length() == 0) fileName = FILENAME;
-		if(!fileName.contains(".")) fileName += ".bin";
-		if(fileName.startsWith(".")) fileName = "users" + fileName;
-		this.fileName = fileName;
+	public UsersCard(int userid, String fn, String ln, String uname, String pass) {
+		load(userid, fn, ln, uname, pass);
 	}
 	
-	public void load() {
-		if(!(new File(fileName)).exists()) return;
-		
-		RandomAccessFile raf = null;
-		clearUsers();
+	public void load(int userid, String fn, String ln, String uname, String pass) {
 		try {
-			raf = new RandomAccessFile(fileName, "r");
-			while(raf.getFilePointer() < raf.length())
-				users.add(new User(raf));
-		} catch (Exception e) {
-			System.out.println("Error with the Random Access File");
-		} finally {
-			try {raf.close();} catch (Exception e) {}
+			FileWriter saveFile = new FileWriter("userDatabaseFile.txt", true);
+			BufferedWriter save = new BufferedWriter(saveFile);
+			save.write(userid + "\t");
+			save.write(fn +"\t");
+			save.write(ln +"\t");
+			save.write(uname +"\t");
+			save.write(pass +"\t");
+			save.newLine();
+			save.close();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
 		}
 	}
 	
-	public void addUser(int userid, String fn, String ln, String uname, String pass) {
-		new User(userid, fn, ln, uname, pass);
-	}
-	
-	public void clearUsers() {
-		users.clear();
-	}
-	
-	
-	@Override
-	public String toString() {
-		String ret = "";
-		for(User s : users) {
-			ret += String.format("%s\n", s);
-		}
-		return ret;
-	}
+
+
 
 }
